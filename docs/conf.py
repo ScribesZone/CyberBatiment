08d9a68deac87b81f8afeeaf2a94f78634b78ff2
escribis <escribis@users.noreply.github.com>
2017-02-13 14:58:51 +0100

    Elimine le repertoire audits de la compil

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,10 @@ release = '1.0.3'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = [
+    '_build',
+    'res/audits'
+]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
